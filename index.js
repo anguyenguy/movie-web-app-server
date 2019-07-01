4ee901309c140d047516f190bc3dc66a0f013ed7
kwon <hdragon1501@gmail.com>
2019-07-01 18:56:35 +0700

    make it better

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const rentals = require('./routes/rentals');
 const express = require('express');
 const app = express();
 
-mongoose.connect('mongodb://localhost/vidly')
+mongoose.connect('mongodb://localhost/movie-server')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
